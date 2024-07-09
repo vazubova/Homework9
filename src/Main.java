89a8b5f3a135d69a5fc3156f8befac2ba7c48f62
varvaragr <grechishnikova.varvara@gmail.com>
2024-07-09 11:56:13 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] payments = {70, 498, 24, 11, 39};
+        int sum = 0;
+        for (int i = 0; i < payments.length; i++) {
+            sum += payments[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum);
+
+        int maxPayment = -1;
+        int minPayment = 1000;
+        for (int j = 0; j < payments.length; j++) {
+            if (payments[j] > maxPayment) {
+                maxPayment = payments[j];
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю сотсавила " + maxPayment + " рублей");
+
+        for (final int current : payments) {
+            if (current < minPayment) {
+                minPayment = current;
+            }
+
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minPayment + " рублей");
+
+        double averageSpending = sum / payments.length;
+        System.out.println("Средняя сумма трат за месяц составила " + averageSpending + " рублей");
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+            if (i > 0) {
+                System.out.print(" ");
+
+            }
+
+
+        }
     }
 }
